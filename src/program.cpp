d0af6e316de662f2c655426818f41d3d393229e5
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2023-06-13 00:08:47 +0200

    add:loader

--- src/program.cpp
+++ src/program.cpp
@@ -8,6 +8,20 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
         Logger::log(LogLevel::Debug, current_path().wstring());
+        Logger::log(LogLevel::Info, TEXT("Initializing..."));
+        if (!exists("./mods/"))
+        {
+            Logger::log(LogLevel::Info, TEXT("Creating mods directory"));
+            create_directory("./mods/");
+        }
+        for (const directory_entry &entry : directory_iterator("./mods/"))
+        {
+            if (entry.path().extension() == ".dll")
+            {
+                Logger::log(LogLevel::Info, TEXT("Loading mod : ") + entry.path().wstring());
+                LoadModule((LPCSTR)entry.path().c_str(), NULL);
+            }
+        }
     }
 
     return TRUE;
